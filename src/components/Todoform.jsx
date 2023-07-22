786230d7a05ab0aec3876cbd695e7ab59fbbacd4
Mo-fopefoluwa <fopefaokunla@gmail.com>
2023-07-22 11:38:27 +0100

    fops

--- src/components/Todoform.jsx
+++ src/components/Todoform.jsx
@@ -1,23 +1,29 @@
-import React, {useState} from 'react'
+import React, { useState } from "react";
 
-function Todoform({addTodo}) {
-    const [value, setValue] = useState("")
+function Todoform({ addTodo }) {
+  const [value, setValue] = useState("");
 
-    const handleSubmit = e => {
-        e.preventDefault();
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-        addTodo(value);
+    addTodo(value);
 
-        setValue("")
-    }
+    setValue("");
+  };
   return (
-    <form className='TodoForm  ' onSubmit={handleSubmit}>
-        <input type='text' className='todo-input' value={value}
-        placeholder='What is the task today?'
-         onChange={(e)=> setValue(e.target.value)}/>
-        <button  type='submit' className='todo-btn '>Add Task</button>
+    <form className="TodoForm " onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className="todo-input"
+        value={value}
+        placeholder="What is the task today?"
+        onChange={(e) => setValue(e.target.value)}
+      />
+      <button type="submit" className="todo-btn ">
+        Add Task
+      </button>
     </form>
-  )
+  );
 }
 
-export default Todoform
+export default Todoform;
